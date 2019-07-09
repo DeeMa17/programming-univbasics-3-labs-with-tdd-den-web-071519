ed7650fc510eb8e168da211fdcc31317ab9723a1
Matt Deason <d.sonmd@gmail.com>
2019-07-09 19:36:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,20 @@
+class calculator.rb do
+end
 
+describe calculator.rb do
+  expect(first_number).to be_an(Integer).or be_a(Float)
+
+  first_number == x
+  second_number == y
+  x != 0 
+  y != 0 
+  
+end 
+
+  do 
+  x + y
+  x - y
+  x * y
+  x / y
+ 
+end
