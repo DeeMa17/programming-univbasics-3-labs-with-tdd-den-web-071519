9ef066a24cca20f2c0d94413d5735b824d99025d
Matt Deason <d.sonmd@gmail.com>
2019-07-09 19:06:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,8 +2,10 @@ class ./calculator.rb do
 end
 
 describe ./calculator.rb do
-  first_number == x (x != 0)
-  second_number == y (y != 0)
+  first_number == x
+  second_number == y
+  x != 0 
+  y != 0 
   x + y
   x - y
   x * y
