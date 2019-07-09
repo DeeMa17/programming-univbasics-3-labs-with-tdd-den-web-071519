ec6b008a93cfbda541d29c4e342b7549b50666ca
Matt Deason <d.sonmd@gmail.com>
2019-07-09 19:30:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,12 +3,14 @@ end
 
 describe calculator.rb do
   expect(first_number).to be_an(Integer).or be_a(Float)
-end
+
   first_number == x
   second_number == y
   x != 0 
   y != 0 
-end
+  
+end 
+
   do 
   x + y
   x - y
